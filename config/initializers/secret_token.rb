aa55045bc161259c5e88075e79965ddeed9c72f4
Matthias Grundmann <matthias.grundmann@student.kit.edu>
2013-04-22 16:41:41 +0200

    initialize with project containing only static contents

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServeTheCityKarlsruhe::Application.config.secret_token = '2399366066b11d32aae2ea80a890675e63056c04ce127d10f0297cf2fde6de3ea1ff4aba46bdbce7500a59337cf71eb0f393a832e62daf18f74553544ae96210'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ServeTheCityKarlsruhe::Application.config.session_store :cookie_store, key: '_serve_the_city_karlsruhe_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ServeTheCityKarlsruhe::Application.config.session_store :active_record_store

f1ae4b0fc19ae92a2570d1b9cd56641edebb2d88
Matthias Grundmann <matthias.grundmann@student.kit.edu>
2013-05-25 12:43:22 +0200

    Set action mailer default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,5 @@ ServeTheCityKarlsruhe::Application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'servethecity-karlsruhe.de' }
 end

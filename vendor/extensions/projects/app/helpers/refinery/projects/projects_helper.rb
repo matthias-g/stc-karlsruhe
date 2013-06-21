10e590ec64d8207b4b48a136482330560c6ac997
Matthias Grundmann <matthias.grundmann@student.kit.edu>
2013-06-21 19:15:13 +0200

    Link to users using their id if the username contains a space Don't show attributes of project if they are empty

--- vendor/extensions/projects/app/helpers/refinery/projects/projects_helper.rb
+++ vendor/extensions/projects/app/helpers/refinery/projects/projects_helper.rb
@@ -3,4 +3,12 @@ module Refinery::Projects::ProjectsHelper
   def missing_volunteer_count
     @project.max_volunteer_count - @project.volunteers.count
   end
+
+  def linkname(user)
+    if user.username.match(/\s/)
+      user.id
+    else
+      user.username
+    end
+  end
 end

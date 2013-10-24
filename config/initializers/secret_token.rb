8898772556704d5da0d4ec90be9ae1f59e8cf7f4
Matthias Grundmann <matthias.grundmann@student.kit.edu>
2013-10-24 13:13:07 +0200

    Add files for initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServethecityKarlsruhe::Application.config.secret_token = '103ea7a6cf316f1ed4e5a7de4c08e06b8490b21b1695833b876f86188e118537361d510b9bcb33a55ba5988a5986706e2512f2e405328b9e21572902f0297806'

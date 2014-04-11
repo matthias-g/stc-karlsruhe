2de9344b210cc5aae6f9f537eca3e65eda74df43
Matthias Grundmann <matthias@glasmail.de>
2014-04-12 01:21:38 +0200

    Configure mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,8 @@ StcKarlsruhe::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'servethecity-karlsruhe.de' }
 end

47453cc4d4d33eb19438071c6ba3cf972bde2985
Matthias Grundmann <matthias@glasmail.de>
2014-04-12 22:18:51 +0200

    Link public/uploads

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :linked_files, %w{.env}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{public/upload}
+set :linked_dirs, %w{public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }

c31f5ffb3365816b46d6084810ea2f908c409df6
Tim 'S.D.Eagle' Zeitz <dev.tim.zeitz@gmail.com>
2014-04-12 19:56:37 +0200

    reverting by refactoring broken migrations see d350d7ed

--- db/migrate/20140408160604_create_join_table_project_volunteer.rb
+++ db/migrate/20140408160604_create_join_table_project_volunteer.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableProjectVolunteer < ActiveRecord::Migration
   def change
     create_join_table :projects, :users, {:table_name => 'projects_volunteers' } do |t|
-      t.contact [:project_id, :user_id]
+      t.index [:project_id, :user_id]
       # t.contact [:user_id, :project_id]
     end
   end
--- db/migrate/20140410101251_create_join_table_users_roles.rb
+++ db/migrate/20140410101251_create_join_table_users_roles.rb
@@ -1,7 +1,7 @@
 class CreateJoinTableUsersRoles < ActiveRecord::Migration
   def change
     create_join_table :users, :roles do |t|
-      t.contact [:user_id, :role_id]
+      t.index [:user_id, :role_id]
     end
   end
 end

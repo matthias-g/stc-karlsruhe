6f0fc10b130d181fd419092225a4a65373941385
Matthias Grundmann <matthias@glasmail.de>
2014-04-13 02:13:11 +0200

    Change migration to work with sqlite and comment out postgres version

--- db/migrate/20140412231138_change_desired_team_size_column_type.rb
+++ db/migrate/20140412231138_change_desired_team_size_column_type.rb
@@ -1,9 +1,15 @@
 class ChangeDesiredTeamSizeColumnType < ActiveRecord::Migration
-  def up
-    execute 'ALTER TABLE projects ALTER COLUMN desired_team_size TYPE integer USING (desired_team_size::integer)'
+
+  def change
+    change_column :projects, :desired_team_size, :integer
   end
 
-  def down
-    execute 'ALTER TABLE projects ALTER COLUMN desired_team_size TYPE text USING (latitude::text)'
-  end
+  # use the following methods instead of the above for a postgres database
+  # def up
+  #   execute 'ALTER TABLE projects ALTER COLUMN desired_team_size TYPE integer USING (desired_team_size::integer)'
+  # end
+  #
+  # def down
+  #   execute 'ALTER TABLE projects ALTER COLUMN desired_team_size TYPE text USING (latitude::text)'
+  # end
 end

40e33c0fe2f346679c24137bf3a194e31f8629f1
Matthias Grundmann <matthias@glasmail.de>
2014-04-13 02:06:05 +0200

    Change migration to work with postgres

--- db/migrate/20140412231138_change_desired_team_size_column_type.rb
+++ db/migrate/20140412231138_change_desired_team_size_column_type.rb
@@ -1,5 +1,9 @@
 class ChangeDesiredTeamSizeColumnType < ActiveRecord::Migration
-  def change
-    change_column :projects, :desired_team_size, :integer
+  def up
+    execute 'ALTER TABLE projects ALTER COLUMN desired_team_size TYPE integer USING (desired_team_size::integer)'
+  end
+
+  def down
+    execute 'ALTER TABLE projects ALTER COLUMN desired_team_size TYPE text USING (latitude::text)'
   end
 end

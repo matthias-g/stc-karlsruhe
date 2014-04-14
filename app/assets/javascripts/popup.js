c8e5920d43b958cd5000f975dee2f4fac54a7a86
josua <web@josua.stabenow.info>
2014-04-14 18:12:30 +0200

    Added popup functionality.

--- app/assets/javascripts/popup.js
+++ app/assets/javascripts/popup.js
@@ -12,4 +12,40 @@ $(document).ready(function() {
             return false;
         });
     });
+
+    // define helper functions
+    function showPopup() {
+        $('#popup').fadeIn().css({display: ''});
+        return false;
+    }
+    function hidePopup() {
+        $('#popup').fadeOut();
+        return false;
+    }
+    function autoFocus() {
+        $('#popup').find('input:not([type="hidden"]), textarea').eq(0).focus();
+    }
+
+    // add popup to html
+    if ($('#popup').size() == 0)
+        $('body').append($('<div id="popup" style="display:none">'
+            +'<div id="popupMiddle"><div id="popupInner"></div></div></div>'));
+    // hide popup on clicking aside
+    $('#popupMiddle').click(hidePopup).children().click(function(e) {
+        e.stopPropagation();
+    });
+    // open popup links in popup
+    $('a.openInPopup').click(function() {
+        var link = $(this),
+            closeButton = $('<a id="popupClose" alt="Abbrechen" href="#"></a>').click(hidePopup),
+            popup = $('#popupInner').empty().append(closeButton),
+            url = link.attr('href') + ' #popupTarget';
+        showPopup();
+        if (link.hasClass('dualPopup')) {
+            var cols = $('<div class="two-col-group">').appendTo(popup);
+            $('<div>').appendTo(cols).load(url, autoFocus);
+            $('<div>').appendTo(cols).load(url);
+        } else popup.load(url, autoFocus);
+        return false;
+    });
 });

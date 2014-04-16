aea20119c983d2b07b6820ff831ea85609227b09
Jonathan K <Jonathan.Kieling@>
2014-04-16 20:59:47 +0200

    Mostly seperated user from profile except in view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,65 +9,25 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def my_profile
-    @user = current_user
-  end
-
   # GET /users/1
   # GET /users/1.json
   def show
-    set_user
-    if current_user == @user
-        redirect_to profile_path
-    end
   end
 
-  # # GET /users/new
-  # def new
-  #   @user = User.new
-  # end
-
-  def edit_my_profile
+  def edit
     @user = current_user
   end
 
-  # # POST /users
-  # # POST /users.json
-  # def create
-  #   @user = User.new(user_params)
-  #
-  #   respond_to do |format|
-  #     if @user.save
-  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
-  #       format.json { render action: 'show', status: :created, location: @user }
-  #     else
-  #       format.html { render action: 'new' }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  def update_my_profile
+  def update
     @user = User.find(current_user.id)
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to action: :my_profile, notice: t('user.message.profileUpdated') }
+        format.html { redirect_to action: :show, notice: t('user.message.profileUpdated') }
         format.json { head :no_content }
       else
         format.html { render action: 'edit_my_profile' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy_my_profile
-    @user = User.find(current_user.id)
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to '/' }
-      format.json { head :no_content }
     end
   end
 

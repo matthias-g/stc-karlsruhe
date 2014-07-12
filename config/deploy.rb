7cb19f80a0b96437048926438a12de5c88a2232d
Matthias Grundmann <matthias@glasmail.de>
2014-07-12 18:54:56 +0200

    Update capistrano version lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+# config valid only for Capistrano 3.2
+lock '3.2.1'
 
 set :application, 'serve_the_city_karlsruhe'
 set :repo_url, 'git@github.com:matthias-g/stc-karlsruhe.git'

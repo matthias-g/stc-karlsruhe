f4378ca1d82ef41b0f135898b7af44b6c1b32975
Matthias Grundmann <matthias@glasmail.de>
2015-02-16 22:46:23 +0100

    Run different migrations depending on database type

--- db/migrate/20140412231138_change_desired_team_size_column_type.rb
+++ db/migrate/20140412231138_change_desired_team_size_column_type.rb
@@ -1,15 +1,18 @@
 class ChangeDesiredTeamSizeColumnType < ActiveRecord::Migration
 
   def change
-    change_column :projects, :desired_team_size, :integer
+    if ActiveRecord::Base.connection.adapter_name == 'SQLite'
+      change_column :projects, :desired_team_size, :integer
+    elsif ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
+      reversible do |change|
+        change.up do
+          execute 'ALTER TABLE projects ALTER COLUMN desired_team_size TYPE integer USING (desired_team_size::integer)'
+        end
+        change.down do
+          execute 'ALTER TABLE projects ALTER COLUMN desired_team_size TYPE text USING (latitude::text)'
+        end
+      end
+    end
   end
 
-  # use the following methods instead of the above for a postgres database
-  # def up
-  #   execute 'ALTER TABLE projects ALTER COLUMN desired_team_size TYPE integer USING (desired_team_size::integer)'
-  # end
-  #
-  # def down
-  #   execute 'ALTER TABLE projects ALTER COLUMN desired_team_size TYPE text USING (latitude::text)'
-  # end
 end

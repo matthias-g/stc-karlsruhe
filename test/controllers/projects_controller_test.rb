86ec36e0fe713131c77523f39d1c710c3297b484
Matthias Grundmann <matthias@glasmail.de>
2015-03-05 16:26:00 +0100

    Fix a couple of tests

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -5,41 +5,48 @@ class ProjectsControllerTest < ActionController::TestCase
     @project = projects(:one)
   end
 
-  test "should get contact" do
-    get :contact
-    assert_response :success
-    assert_not_nil assigns(:projects)
-  end
-
-  test "should get new" do
+  test 'should get new' do
+    sign_in users(:sabine)
     get :new
     assert_response :success
   end
 
-  test "should create project" do
+  test 'should create project' do
+    sign_in users(:rolf)
     assert_difference('Project.count') do
-      post :create, project: { description: @project.description, individual_tasks: @project.individual_tasks, latitude: @project.latitude, location: @project.location, longitude: @project.longitude, material: @project.material, requirements: @project.requirements, title: @project.title, visible: @project.visible }
+      post :create, project: { description: 'Description', individual_tasks: 'Tasks', title: 'Title', desired_team_size: '6' }
     end
 
     assert_redirected_to project_path(assigns(:project))
   end
 
-  test "should show project" do
+  test 'should show visible project' do
     get :show, id: @project
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should redirect when trying to show invisible project' do
+    @project = projects(:two)
+    get :show, id: @project
+    assert_response :redirect
+  end
+
+  test 'should get edit' do
+    sign_in users(:rolf)
     get :edit, id: @project
     assert_response :success
   end
 
-  test "should update project" do
-    patch :update, id: @project, project: { description: @project.description, individual_tasks: @project.individual_tasks, latitude: @project.latitude, location: @project.location, longitude: @project.longitude, material: @project.material, requirements: @project.requirements, title: @project.title, visible: @project.visible }
+  test 'should update project' do
+    sign_in users(:rolf)
+    patch :update, id: @project, project: { description: 'New Description', title: @project.title, desired_team_size: @project.desired_team_size }
+    assert_equal I18n.t('project.message.updated'), flash[:notice]
     assert_redirected_to project_path(assigns(:project))
   end
 
-  test "should destroy project" do
+  test 'should destroy project' do
+    sign_in users(:rolf)
+    assert users(:rolf).leads_project? @project
     assert_difference('Project.count', -1) do
       delete :destroy, id: @project
     end

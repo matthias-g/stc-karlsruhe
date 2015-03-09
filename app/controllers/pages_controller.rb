2f30bba37587aad2fc941855fb8da939a1cafc05
Josua <josua.stabenow@gmx.de>
2015-03-09 05:17:53 +0100

    Removed page sections and pages (both are obsolete).

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,46 +1,9 @@
 class PagesController < ApplicationController
-  before_action :set_page, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_admin_user!, only: [:index, :new, :edit, :create, :update, :destroy]
-
   respond_to :html
-
-  def index
-    @pages = Page.all
-    respond_with(@pages)
-  end
-
-  def show
-    respond_with(@page)
-  end
 
   def welcome
     @partial_name = 'welcome'
     render 'page'
-  end
-
-  def new
-    @page = Page.new
-    respond_with(@page)
-  end
-
-  def edit
-    respond_with(@page)
-  end
-
-  def create
-    @page = Page.new(page_params)
-    @page.save
-    respond_with(@page)
-  end
-
-  def update
-    @page.update(page_params)
-    respond_with(@page)
-  end
-
-  def destroy
-    @page.destroy
-    respond_with(@page)
   end
 
   def page
@@ -49,14 +12,9 @@ class PagesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_page
-      @page = Page.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def page_params
-      params.require(:page).permit(:title, { :section_ids => [] }, :header_name, :address)
+      params.require(:page)
     end
 
 end
--- app/models/page.rb
+++
@@ -1,6 +0,0 @@
-class Page < ActiveRecord::Base
-  has_many :sections, :class_name => 'PageSection'
-  accepts_nested_attributes_for :sections
-
-  validates_presence_of :header_name
-end

f8f1cb3d475ebef6d29c099de9dbf86fd2697a5e
Josua <josua.stabenow@gmx.de>
2015-05-08 00:51:03 +0200

    Added  ajax for  loading new weekdays after the week has been changed in the project edit form.

--- app/assets/javascripts/views/projects.js
+++ app/assets/javascripts/views/projects.js
@@ -16,3 +16,14 @@ $(document).ready(function() {
   $('#projectFilter select, #projectFilter input').change(ajaxSubmitter);
   $('#projectFilter input[type="submit"]').hide();
 });
+
+/* Load new weekdays in the project edit view once the week is changed. */
+function updateWeekDays() {
+    var week = $('.week select').get(0).value;
+    $.getJSON('/api/project_weeks/' + week + '/project_days.json', function(res) {
+        var daySelect = $('.days select').empty();
+        $.each(res, function(idx, day) {
+            daySelect.append($('<option value="'+day.id+'">'+day.title+'</option>'));
+        });
+    });
+}

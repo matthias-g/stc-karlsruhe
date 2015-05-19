f4c7a63dc78ab1ab90eccf999d09acdf3c4ab420
Matthias Grundmann <matthias@glasmail.de>
2015-05-20 01:05:03 +0200

    Don't show '+' if no undeleted volunteers participate in project

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -5,7 +5,8 @@ module ProjectsHelper
     deleted_users_count = users.where(cleared: true).count
     output = existing_users.map { |v| link_to(v.first_name , user_path(v)) }.join(', ')
     if deleted_users_count > 0
-      output += ' + ' + deleted_users_count.to_s + ' ' + User.model_name.human(count: deleted_users_count)
+      output += ' + ' if existing_users.count > 0
+      output += deleted_users_count.to_s + ' ' + User.model_name.human(count: deleted_users_count)
     end
     output.html_safe
   end

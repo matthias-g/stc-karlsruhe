2a059b878b1bae11bc73a00ccd09f17910cd9b25
Matthias Grundmann <matthias@glasmail.de>
2015-05-25 23:29:34 +0200

    Sortable occupancy statistics

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -11,7 +11,26 @@ class StatisticsController < ApplicationController
   end
 
   def occupancy
-    @projects = ProjectWeek.default.projects.visible
+    @projects = ProjectWeek.default.projects.visible.sort_by { |project| sort_column(project) }
+    if params[:direction] == 'desc'
+      @projects.reverse!
+    end
+  end
+
+  private
+  def sort_column(project)
+    case params[:sort]
+      when 'volunteer_count'
+        project.volunteers.count
+      when 'free_places'
+        project.desired_team_size - project.volunteers.count
+      when 'desired_team_size'
+        project.desired_team_size
+      when 'occupancy'
+        project.desired_team_size > 0 ? 100 * project.volunteers.count / project.desired_team_size : 0
+      else
+        project.title
+    end
   end
 
 end
--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,2 +1,9 @@
 module StatisticsHelper
+
+  def sortable_column(column, title = nil)
+    title ||= column.titleize
+    direction = (column == params[:sort] && params[:direction] == 'asc') ? 'desc' : 'asc'
+    link_to title, :sort => column, :direction => direction
+  end
+
 end

949b7e46fc9a03d5f6668faa10c92692f611f05e
Matthias Grundmann <matthias@glasmail.de>
2015-05-25 23:38:31 +0200

    Add sort icon to sortable table header

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -3,7 +3,9 @@ module StatisticsHelper
   def sortable_column(column, title = nil)
     title ||= column.titleize
     direction = (column == params[:sort] && params[:direction] == 'asc') ? 'desc' : 'asc'
-    link_to title, :sort => column, :direction => direction
+    link_to title, :sort => column, :direction => direction do
+      (title + " <i class='fa fa-sort'></i>").html_safe
+    end
   end
 
 end

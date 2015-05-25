18dc4da5f89afa1bd5728cff814e5139e2159dd9
Matthias Grundmann <matthias@glasmail.de>
2015-05-25 23:42:16 +0200

    Fix link in header

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -3,7 +3,7 @@ module StatisticsHelper
   def sortable_column(column, title = nil)
     title ||= column.titleize
     direction = (column == params[:sort] && params[:direction] == 'asc') ? 'desc' : 'asc'
-    link_to title, :sort => column, :direction => direction do
+    link_to sort: column, direction: direction do
       (title + " <i class='fa fa-sort'></i>").html_safe
     end
   end

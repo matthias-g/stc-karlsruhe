27c4f367e6b74cc4553ea9b7cb7c29520023fb2b
Matthias Grundmann <matthias@glasmail.de>
2015-05-31 23:39:17 +0200

    Show subprojects and parent project in right column with thumbnails #34912934989151 #34912934989153

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,7 +3,7 @@ module ProjectsHelper
   def list_users(users)
     existing_users = users.where(cleared: false)
     deleted_users_count = users.where(cleared: true).count
-    output = existing_users.map { |v| link_to(v.first_name , user_path(v)) }.join(', ')
+    output = existing_users.map { |v| link_to(v.first_name, user_path(v)) }.join(', ')
     if deleted_users_count > 0
       output += ' + ' if existing_users.count > 0
       output += deleted_users_count.to_s + ' ' + User.model_name.human(count: deleted_users_count)

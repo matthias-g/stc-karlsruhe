9cd7f16afe622cde4d1eed0535b3dcc157fe9c1b
Josua <josua.stabenow@gmx.de>
2015-06-01 11:33:49 +0200

    Added gallery image auto submit.

--- app/assets/javascripts/views/galleries.js
+++ app/assets/javascripts/views/galleries.js
@@ -137,4 +137,15 @@ $(document).ready(function ($) {
         });
     });
 
+
+    // auto submit uploaded images
+    $('form.edit_gallery').each(function(i, f) {
+        $('#gallery_picture', f).change(function() {
+            $('.waitinfo', f).show();
+            $(f).submit();
+        });
+        $('input[type="submit"]', f).css({visibility: 'hidden'});
+    });
+
+
 });

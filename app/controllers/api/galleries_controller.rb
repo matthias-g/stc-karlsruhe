54cb8692c9d61f7e1c110083df405b6bfde945e5
Matthias Grundmann <matthias@glasmail.de>
2015-06-03 00:26:56 +0200

    Working on integrating deletion of gallery pictures into frontend #36078043944757

--- app/controllers/api/galleries_controller.rb
+++ app/controllers/api/galleries_controller.rb
@@ -4,7 +4,16 @@ class Api::GalleriesController < ApplicationController
   respond_to :json
 
   def show
-    @gallery_pictures = @gallery.gallery_pictures.visible_for_user(current_user).to_json(only: [:width, :height, :picture])
+    gallery_pictures = @gallery.gallery_pictures.visible_for_user(current_user).as_json(only: [:width, :height, :picture, :id])
+    if current_user
+      user_is_admin = current_user.is_admin?
+      @gallery.gallery_pictures.visible_for_user(current_user).each_with_index { |picture, index|
+        if picture.uploader_id == current_user.id || user_is_admin
+          gallery_pictures[index][:editable] = true
+        end
+      }
+    end
+    @gallery_pictures = gallery_pictures.to_json
     respond_with(@gallery)
   end
 

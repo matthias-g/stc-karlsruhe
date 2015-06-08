555ef840aeaaf9d9f5268aee498832d893a4bfb3
Matthias Grundmann <matthias@glasmail.de>
2015-06-09 00:11:11 +0200

    Destroy survey answers when destroying survey

--- app/models/feedback/survey.rb
+++ app/models/feedback/survey.rb
@@ -1,5 +1,6 @@
 class Feedback::Survey < ActiveRecord::Base
 
   has_many :questions, -> { order 'position' }, class_name: 'Feedback::Question', dependent: :destroy
+  has_many :survey_answers, :class_name => 'Feedback::SurveyAnswer', dependent: :destroy
   accepts_nested_attributes_for :questions, reject_if: lambda { |a| a[:text].blank? }, allow_destroy: true
 end

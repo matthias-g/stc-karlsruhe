a93c655a55d73c9ee42492369195a95369f2b5bc
Josua <josua.stabenow@gmx.de>
2015-06-09 11:14:51 +0200

    Sorts team members by participation date.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,9 +1,9 @@
 module ProjectsHelper
 
   def list_users(users)
-    existing_users = users.where(cleared: false)
+    existing_users = users.where(cleared: false).select('users.*, participations.created_at AS project_signup')
     deleted_users_count = users.where(cleared: true).count
-    output = existing_users.map { |v| link_to(v.first_name, user_path(v)) }.join(', ')
+    output = existing_users.sort_by{|v| v.project_signup ||= Time.at(0)}.map{|v| link_to(v.first_name, user_path(v)) }.join(', ')
     if deleted_users_count > 0
       output += ' + ' if existing_users.count > 0
       output += deleted_users_count.to_s + ' ' + User.model_name.human(count: deleted_users_count)

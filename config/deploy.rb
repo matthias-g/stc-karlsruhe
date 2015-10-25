d41f8cbfe9f3e4ce9c904320798612dbfa24107b
Matthias Grundmann <matthias@glasmail.de>
2015-10-25 14:49:09 +0100

    Link tmp dirs, too

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :linked_files, %w{.env}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{public/downloads public/uploads log}
+set :linked_dirs, %w{log tmp/pids tmp/cache public/downloads public/uploads}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }

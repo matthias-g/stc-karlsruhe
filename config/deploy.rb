85cba7e81ce5ee0bb492e8a0058d182418913f87
Matthias Grundmann <matthias@glasmail.de>
2015-10-25 17:14:34 +0100

    Disabled preload app

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,6 @@ set :keep_releases, 20
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:legacy_restart'
+    invoke 'unicorn:reload'
   end
 end
--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -4,19 +4,3 @@
 listen 5876 # by default Unicorn listens on port 8080
 worker_processes 12 # this should be >= nr_cpus
 pid '/home/stc/servethecity-karlsruhe/current/tmp/pids/unicorn.pid'
-
-# combine Ruby 2.0.0+ with "preload_app true" for memory savings
-preload_app true
-
-before_fork do |server, worker|
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.connection.disconnect!
-end
-
-after_fork do |server, worker|
-  # the following is *required* for Rails + "preload_app true",
-  defined?(ActiveRecord::Base) and
-      ActiveRecord::Base.establish_connection
-end

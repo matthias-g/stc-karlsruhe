37c2524dc6b7453f4f3c152068cf8f3057a4a28f
Matthias Grundmann <matthias@glasmail.de>
2015-10-25 16:54:22 +0100

    Use unicorn legacy restart to stop old master

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,6 @@ set :keep_releases, 20
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
-    invoke 'unicorn:restart'
+    invoke 'unicorn:legacy_restart'
   end
 end

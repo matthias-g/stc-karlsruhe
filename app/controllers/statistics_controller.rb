775a245d0caf58fc9d0c52346e8bed89cef30639
Matthias Grundmann <matthias@glasmail.de>
2016-06-01 16:35:35 +0200

    Show aggregated statistics for parent projects with team_size == 0

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -22,13 +22,19 @@ class StatisticsController < ApplicationController
   def sort_column(project)
     case params[:sort]
       when 'volunteer_count'
-        project.volunteers.count
+        project.desired_team_size > 0 ? project.volunteers.count : project.aggregated_volunteers.count
       when 'free_places'
-        project.desired_team_size - project.volunteers.count
+        project.desired_team_size > 0 ? project.desired_team_size - project.volunteers.count : project.aggregated_desired_team_size - project.aggregated_volunteers.count
       when 'desired_team_size'
-        project.desired_team_size
+        project.desired_team_size > 0 ? project.desired_team_size : project.aggregated_desired_team_size
       when 'occupancy'
-        project.desired_team_size > 0 ? 100 * project.volunteers.count / project.desired_team_size : 0
+        if project.desired_team_size > 0
+          100 * project.volunteers.count / project.desired_team_size
+        elsif project.aggregated_desired_team_size > 0
+          100 * project.aggregated_volunteers.count / project.aggregated_desired_team_size
+        else
+          0
+        end
       else
         project.title
     end

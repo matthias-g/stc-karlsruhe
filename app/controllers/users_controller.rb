d975768c9775b00e686de3871fa1c34d0c40e825
Matthias Grundmann <matthias@glasmail.de>
2016-06-02 21:58:35 +0200

    Fix if no template found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def show
     if @user.id === current_user.id
       template = Surveys::Template.where(show_in_user_profile: true).order('RANDOM()').first
-      unless template && template.submissions.where(user_id: current_user.id).exists?
+      if template && !template.submissions.where(user_id: current_user.id).exists?
         @submission = Surveys::Submission.create_for_template(template)
       end
     else

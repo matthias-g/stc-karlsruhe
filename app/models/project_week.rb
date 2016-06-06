93f33b8791a507b96339f2290b7136ab08441f4c
Josua <josua.stabenow@gmx.de>
2016-06-06 12:42:34 +0200

    Added new logo without date for FB sharing. Added project week date range in meta description.

--- app/models/project_week.rb
+++ app/models/project_week.rb
@@ -14,4 +14,17 @@ class ProjectWeek < ActiveRecord::Base
     projects.active.toplevel.map{|p| p.aggregated_desired_team_size - p.aggregated_volunteers.count}.sum
   end
 
+  def date_range
+    default = Date.new(1970, 1, 1)
+    return default..default if days.empty?
+    min = max = days.first.date
+    days.each do |day|
+      d = day.date
+      return default..default if d.blank?
+      min = d if d < min
+      max = d if d > max
+    end
+    return min..max
+  end
+
 end

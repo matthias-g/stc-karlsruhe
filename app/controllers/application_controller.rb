f81d1d0f1a88e7357517dfbe5a3f0112de931635
Matthias Grundmann <matthias@glasmail.de>
2016-07-06 23:03:39 +0200

    Update devise parameter sanitizer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(
-        :username, :email, :first_name, :last_name,:password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(
-        :login, :username, :email, :password) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
-        :username, :email, :first_name, :last_name, :phone,
-        :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :email, :first_name, :last_name,:password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :login, :username, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :username, :email, :first_name, :last_name, :phone,
+                                                               :password, :password_confirmation, :current_password])
   end
 
   def not_found

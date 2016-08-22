d721c4aac7caef2c30f85ad5b8975cb741735d4e
Matthias Grundmann <matthias@glasmail.de>
2016-08-22 12:20:04 +0200

    Update gems

--- app/assets/javascripts/editing.js
+++ app/assets/javascripts/editing.js
@@ -2,7 +2,7 @@
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 
 //= require bootstrap-select
-//= require bootstrap-select/i18n/defaults-de_DE
+//= require i18n/defaults-de_DE
 //= require jquery.Jcrop
 //= require highcharts
 //= require chartkick

67615070b2d595dbfc39a5fdbd6a75182c31f0e9
Matthias Grundmann <matthias@glasmail.de>
2017-03-27 23:39:10 +0200

    Improve live search of role picker

--- app/helpers/roles_helper.rb
+++ app/helpers/roles_helper.rb
@@ -1,7 +1,8 @@
 module RolesHelper
   def options_for_add_user_to_role(role)
     options = policy_scope(User.all.where(cleared: false)).order(:first_name, :last_name).map do |user|
-      [user.full_name, user.id, {data: { tokens: user.username }}]
+      tokens = user.username + ' ' + user.first_name + ' ' + user.last_name
+      [user.full_name, user.id, { data: { tokens: tokens } }]
     end
     options_for_select(options)
   end

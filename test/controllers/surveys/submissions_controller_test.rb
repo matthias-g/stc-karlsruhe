12881d9e4b0ab4b9484005a1beb6472b62802981
Matthias Grundmann <matthias@glasmail.de>
2017-08-18 16:01:53 +0200

    Include UTF-8 BOM in exported csv #137207392159570 see https://en.wikipedia.org/wiki/Byte_order_mark#UTF-8

--- test/controllers/surveys/submissions_controller_test.rb
+++ test/controllers/surveys/submissions_controller_test.rb
@@ -37,6 +37,25 @@ class Surveys::SubmissionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should show surveys_submission as csv" do
+    sign_in users(:admin)
+    get surveys_template_surveys_submissions_url(@surveys_submission.template_id, format: :csv)
+    assert_response :success
+  end
+
+  test "csv should start with UTF-8 BOM" do
+    sign_in users(:admin)
+    get surveys_template_surveys_submissions_url(@surveys_submission.template_id, format: :csv)
+    assert response.body.start_with?("\xEF\xBB\xBF")
+  end
+
+  test "csv should use ; as separator" do
+    sign_in users(:admin)
+    template = @surveys_submission.template
+    get surveys_template_surveys_submissions_url(template.id, format: :csv)
+    assert response.body.lines.first.include?("\"#{template.questions.first.text}\";\"#{template.questions.second.text}\"")
+  end
+
   test "should not get edit" do
     sign_in users(:admin)
     get edit_surveys_template_surveys_submission_url(@surveys_submission.template_id, @surveys_submission)

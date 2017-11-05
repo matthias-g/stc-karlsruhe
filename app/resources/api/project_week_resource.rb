b04f9f9a486bb4d9e030b6387767fb1c84835336
Matthias Grundmann <matthias@glasmail.de>
2017-11-05 12:47:30 +0100

    API: Add calculated stats to project week

--- app/resources/api/project_week_resource.rb
+++ app/resources/api/project_week_resource.rb
@@ -2,6 +2,15 @@ class Api::ProjectWeekResource < JSONAPI::Resource
   include JSONAPI::Authorization::PunditScopedResource
 
   attribute :title
+  attributes :stats
   has_many :days, class_name: 'ProjectDay'
   has_many :projects
+
+  def stats
+    {
+        'project_count': @model.projects.visible.where.not(desired_team_size: 0).count,
+        'active_user_count': @model.active_user_count,
+        'vacancy_count': @model.vacancy_count
+    }
+  end
 end

9f9a6e89f3f1e79e109b56a14a31f0f32baa7d3d
Matthias Grundmann <matthias@glasmail.de>
2018-04-17 21:42:21 +0200

    Send orga mails in batches of 400

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -35,50 +35,7 @@ class Mailer < ActionMailer::Base
     mail to: recipient.email, reply_to: sender.email, subject: message.subject
   end
 
-  def orga_mail(message)
-    current_actions = ActionGroup.default.actions.visible
-    case message.recipient
-      when 'current_volunteers_and_leaders'
-        to = current_actions.joins(:events).joins('LEFT OUTER JOIN participations ON events.id = participations.event_id')
-                 .joins('LEFT OUTER JOIN leaderships ON actions.id = leaderships.action_id')
-                 .joins('INNER JOIN "users" ON "users"."id" = "participations"."user_id" OR "users"."id" = "leaderships"."user_id"')
-                 .where('users.cleared': false)
-      when 'current_volunteers'
-        to = current_actions.joins(:events)
-                 .joins('JOIN participations ON events.id = participations.event_id')
-                 .joins('JOIN users ON users.id = participations.user_id')
-                 .where('users.cleared': false)
-      when 'current_leaders'
-        to = current_actions.joins(:leaders).where('users.cleared': false)
-      when 'all_users'
-        to = User.where(cleared: false).all
-      when 'active_users'
-        to = User.where(cleared: false)
-                 .joins('LEFT JOIN participations on participations.user_id = users.id')
-                 .where('participations.created_at > ? or users.created_at > ?', 18.months.ago, 6.months.ago)
-      when 'me'
-        to = User.where(id: message.sender.id)
-      else
-        to = message.recipient.split(/\s*,\s*/)
-    end
-    unless to.kind_of?(Array)
-      case message.content_type
-        when 'about_action_groups'
-          if %w(current_volunteers_and_leaders current_volunteers current_leaders).include? message.recipient
-            to = to.where('users.receive_emails_about_my_action_groups': true)
-          else
-            to = to.where('users.receive_emails_about_action_groups': true)
-          end
-        when 'about_other_actions'
-          to = to.where('users.receive_emails_about_other_actions': true)
-        when 'other_email_from_orga'
-          to = to.where('users.receive_other_emails_from_orga': true)
-        else
-          to = to.where('users.receive_other_emails_from_orga': true)
-      end
-      to = to.pluck(:email)
-    end
-    recipients = (to + [message.sender.email]).uniq.join(',')
+  def orga_mail(message, recipients)
     @message = message.body
     @type = message.content_type
     mail reply_to: message.from, bcc: recipients, subject: message.subject
--- app/models/orga_message.rb
+++ app/models/orga_message.rb
@@ -10,12 +10,65 @@ class OrgaMessage < ApplicationRecord
     sent_at != nil
   end
 
-  def send_message sender
+  def send_message(sender)
     self.sender = sender
-    Mailer.orga_mail(self).deliver_now
+    send_message_in_batches
     self.sent_at = Time.now
-    self.recipient = render_recipient_group self.recipient, sender
+    self.recipient = render_recipient_group(self.recipient, sender)
     self.save
   end
+
+  private
+
+  # TODO this could be moved to a service
+  def send_message_in_batches
+    current_actions = ActionGroup.default.actions.visible
+    case self.recipient
+    when 'current_volunteers_and_leaders'
+      to = current_actions.joins(:events).joins('LEFT OUTER JOIN participations ON events.id = participations.event_id')
+               .joins('LEFT OUTER JOIN leaderships ON actions.id = leaderships.action_id')
+               .joins('INNER JOIN "users" ON "users"."id" = "participations"."user_id" OR "users"."id" = "leaderships"."user_id"')
+               .where('users.cleared': false)
+    when 'current_volunteers'
+      to = current_actions.joins(:events)
+               .joins('JOIN participations ON events.id = participations.event_id')
+               .joins('JOIN users ON users.id = participations.user_id')
+               .where('users.cleared': false)
+    when 'current_leaders'
+      to = current_actions.joins(:leaders).where('users.cleared': false)
+    when 'all_users'
+      to = User.where(cleared: false).all
+    when 'active_users'
+      to = User.where(cleared: false)
+               .joins('LEFT JOIN participations on participations.user_id = users.id')
+               .where('participations.created_at > ? or users.created_at > ?', 18.months.ago, 6.months.ago)
+    when 'me'
+      to = User.where(id: self.sender.id)
+    else
+      to = self.recipient.split(/\s*,\s*/)
+    end
+    unless to.kind_of?(Array)
+      case self.content_type
+      when 'about_action_groups'
+        if %w(current_volunteers_and_leaders current_volunteers current_leaders).include?(self.recipient)
+          to = to.where('users.receive_emails_about_my_action_groups': true)
+        else
+          to = to.where('users.receive_emails_about_action_groups': true)
+        end
+      when 'about_other_actions'
+        to = to.where('users.receive_emails_about_other_actions': true)
+      when 'other_email_from_orga'
+        to = to.where('users.receive_other_emails_from_orga': true)
+      else
+        to = to.where('users.receive_other_emails_from_orga': true)
+      end
+      to = to.pluck(:email)
+    end
+    recipients = (to + [self.sender.email]).uniq
+    recipients.in_groups_of(400) do |some_recipients|
+      Mailer.orga_mail(self, some_recipients.join(',')).deliver_now
+    end
+  end
+
 
 end

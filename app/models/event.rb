b1a3f896e1f7746d899237a986021be6c4f0cf89
Matthias Grundmann <matthias@glasmail.de>
2018-05-09 21:14:19 +0200

    Fix counter_cache for team_size

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
 
   belongs_to :initiative, class_name: 'Action'
 
-  has_many :participations, dependent: :destroy
+  has_many :participations, dependent: :destroy, counter_cache: :team_size
   has_many :volunteers, class_name: 'User', through: :participations, source: :user,
            after_add: :on_volunteer_added, after_remove: :on_volunteer_removed
 

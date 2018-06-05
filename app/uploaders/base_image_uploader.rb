6a91b9f59f0edeee354bd222bf1a3512ccc466dd
Josua Stabenow <josua.stabenow@gmx.de>
2018-06-05 13:40:26 +0200

    Fix picture cropping

--- app/uploaders/base_image_uploader.rb
+++ app/uploaders/base_image_uploader.rb
@@ -19,12 +19,13 @@ class BaseImageUploader  < CarrierWave::Uploader::Base
   # will crop the version according to the values given in model.crop_x/y/w/h
   def crop
     if model.crop_x.present?
-      resize_to_limit(500, 500)
+      real_w, real_h = ::MiniMagick::Image.open(model.picture.file.file)[:dimensions]
+      precrop_w, precrop_h = ::MiniMagick::Image.open(model.picture.precrop.file.file)[:dimensions]
       manipulate! do |img|
-        x = model.crop_x
-        y = model.crop_y
-        w = model.crop_w
-        h = model.crop_h
+        x = model.crop_x / precrop_w * real_w
+        y = model.crop_y / precrop_h * real_h
+        w = model.crop_w / precrop_w * real_w
+        h = model.crop_h / precrop_h * real_h
         img.crop "#{w}x#{h}+#{x}+#{y}"
         img
       end

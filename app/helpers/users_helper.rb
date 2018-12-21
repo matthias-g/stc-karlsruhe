a0ce3ff9047558e5aea0b78d744a2e003e67b328
Josua Stabenow <josua.stabenow@gmx.de>
2018-12-21 17:42:30 +0100

    Fix translations

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,22 +1,3 @@
 module UsersHelper
-
-  # Infers the value of the "Receive orga mails" checkbox in the user profile edit page TODO this is outdated since subscriptions
-  def receive_email_from_orga user
-    user.receive_emails_about_action_groups || user.receive_emails_about_my_action_groups ||
-        user.receive_emails_about_other_projects || user.receive_other_emails_from_orga
-  end
-
-  # Summarizes orga mail settings for the user profile page
-  def email_reception_description user
-    if receive_email_from_orga(user) && user.receive_emails_from_other_users
-      t('user.info.emailReception.orgaAndUser')
-    elsif receive_email_from_orga user
-      t('user.info.emailReception.orga')
-    elsif user.receive_emails_from_other_users
-      t('user.info.emailReception.user')
-    else
-      t('user.info.emailReception.none')
-    end
-  end
   
 end

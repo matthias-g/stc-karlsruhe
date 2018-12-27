a1ec8a27ef452796a3cc6d8f7af59ac5cc0c3bfa
Matthias Grundmann <matthias@glasmail.de>
2018-12-27 22:09:31 +0100

    Add missing subscription test

--- spec/policies/subscription_policy_spec.rb
+++ spec/policies/subscription_policy_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe SubscriptionPolicy do
 
   let(:user) { nil }
   let(:record) { subscriptions(:volunteer) }
+  let(:policy) { SubscriptionPolicy.new(user, record) }
 
   permissions :create? do
     subject { described_class }
@@ -29,6 +30,15 @@ RSpec.describe SubscriptionPolicy do
     context 'as same user' do
       let(:user) { users(:volunteer) }
       it { grants_access }
+    end
+  end
+
+  describe 'updatable_fields' do
+    subject { policy.updatable_fields }
+    let(:all_fields) { Api::SubscriptionResource._updatable_relationships | Api::SubscriptionResource._attributes.keys - [:id] }
+
+    it 'contains all attributes except confirmed_at' do
+      expect(subject).to match_array(all_fields - [:confirmed_at])
     end
   end
 
